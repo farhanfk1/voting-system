012ede39b0b709a83ae4ca9b7de17c207b31c452
farhanfk1 <farhankhanbamkhel170@gmail.com>
2025-09-05 05:06:17 -0700

    utils message

---
+++ voting_system/lib/utils/utils.dart
@@ -0,0 +1,49 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Utils {
+
+   static toastMessage(String message){
+  Fluttertoast.showToast(
+    msg: message,
+    backgroundColor: Colors.black,
+    textColor: Colors.white,
+    toastLength: Toast.LENGTH_LONG
+    );
+}
+   
+   static void flushBarErrorMEssage(String message, BuildContext context){
+      
+         showFlushbar(
+          context: context, 
+          flushbar: Flushbar(
+            forwardAnimationCurve: Curves.decelerate,
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            padding: EdgeInsets.all(15),
+            message: message,
+            backgroundColor: Colors.red,
+            title: "!",
+            messageColor: Colors.white,
+            duration: Duration(seconds: 3),
+            flushbarPosition: FlushbarPosition.TOP,
+            reverseAnimationCurve: Curves.easeInOut,
+            positionOffset: 20,
+            icon: Icon(Icons.error, size: 28, color: Colors.white,),
+          )..show(context)
+          );
+
+   }
+
+
+   static snakBar(String message, BuildContext context){
+
+    return  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      backgroundColor: Colors.red,
+      content: Text(message)
+      ));
+
+   }
+
+}
